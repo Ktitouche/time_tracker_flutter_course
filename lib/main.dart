29409ddc1ba8e4913148e3a9af959d752e586a27
clio7amra <ktitouche404@gmail.com>
2024-10-23 21:38:23 +0100

    wlh je tesete ne me hagar pas

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+  primarySwatch;
 }
 
 class MyApp extends StatelessWidget {
